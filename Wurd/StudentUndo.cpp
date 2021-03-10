a274d20f8d3b0207fec25f531194b765d99568a1
Justin Hee <justinhee118@gmail.com>
2021-03-09 23:28:33 -0800

    rewrote undo for insert

--- Wurd/StudentUndo.cpp
+++ Wurd/StudentUndo.cpp
@@ -14,43 +14,57 @@ StudentUndo::Action StudentUndo::get(int& row, int& col, int& count, std::string
     if(m_stack.empty())
         return Action::ERROR;  // TODO'
 
-    UndoAction top = m_stack.top();
+    UndoAction curr = m_stack.top();
     m_stack.pop();
     
 
-    row = top.m_row;
-    col = top.m_col;
+    row = curr.m_row;
+    col = curr.m_col;
     count = 1;
     text = "";
     
-    if(top.m_action == Action::INSERT)
+    if(curr.m_action == Action::INSERT)
     {
         //if there's more undo actions, check if they're inserts
-        if(!m_stack.empty())
+//        if(!m_stack.empty())
+//        {
+//            UndoAction next = m_stack.top();
+//            //get all the consecutive inserts and add to the count of deletes
+//            while(next.m_action == Action::INSERT && next.m_row == curr.m_row && next.m_col == curr.m_col-1)
+//            {
+//                count++;
+//                m_stack.pop();
+//                curr = next;
+//
+//                if(!m_stack.empty())
+//                    next = m_stack.top();
+//            }
+//        }
+        //if there's more undo actions, check if they're inserts
+        while(!m_stack.empty())
         {
             UndoAction next = m_stack.top();
-            //get all the consecutive inserts and add to the count of deletes
-            while(next.m_action == Action::INSERT && next.m_row == top.m_row && next.m_col == top.m_col-1)
+            //if the next action is an insert and next to the current one,
+            if(next.m_action == Action::INSERT && next.m_row == curr.m_row && next.m_col == curr.m_col-1)
             {
                 count++;
                 m_stack.pop();
-                top = next;
-                
-                if(!m_stack.empty())
-                    next = m_stack.top();
+                curr = next;
             }
+            else
+                break;
         }
         return Action::DELETE;
     }
-    if(top.m_action == Action::DELETE)
+    if(curr.m_action == Action::DELETE)
     {
-        text += top.m_ch;
+        text += curr.m_ch;
         UndoAction next = m_stack.top();
         //get all the consective deletes/backspaces and add the characters to the text string
-        while(!m_stack.empty() && next.m_action == Action::DELETE && next.m_row == top.m_row && (next.m_col == top.m_col && next.m_col == top.m_col+1))
+        while(!m_stack.empty() && next.m_action == Action::DELETE && next.m_row == curr.m_row && (next.m_col == curr.m_col && next.m_col == curr.m_col+1))
         {
             //this means it was a delete, so add the char to the end
-            if(next.m_col  == top.m_col)
+            if(next.m_col  == curr.m_col)
             {
                 text += next.m_ch;
             }
@@ -60,7 +74,7 @@ StudentUndo::Action StudentUndo::get(int& row, int& col, int& count, std::string
                 text = next.m_ch + text;
             }
             m_stack.pop();
-            top = next;
+            curr = next;
             next = m_stack.top();
 
         }
@@ -69,7 +83,7 @@ StudentUndo::Action StudentUndo::get(int& row, int& col, int& count, std::string
     }
     
     
-    switch (top.m_action) {
+    switch (curr.m_action) {
         case Undo::INSERT:
             return Action::DELETE;
             break;
